41e6081c4eaa56599e95b832e72423b7a60238b8
LukeH1993 <lwheudebourck@gmail.com>
2017-10-23 15:16:50 +0100

    Got all_beers, single_beer and random_beer working

--- punk_services.rb
+++ punk_services.rb
@@ -1,8 +1,36 @@
 require 'httparty'
 require 'json'
 
-class PunkServices
+class PunkService
   include HTTParty
 
-  base uri 'https://api.punkapi.com/v2/beers'
+  base_uri 'https://api.punkapi.com/v2'
+
+  # def parameters
+  #   JSON.parse(self.class.get("/beers?brewed_before=11-2012&abv_gt=6").body)
+  # end
+  #
+  # def pagination
+  #   JSON.parse(self.class.get("/beers?page=2&per_page=80").body)
+  # end
+
+  def all_beers
+    JSON.parse(self.class.get("/beers").body)
+  end
+
+  def single_beer(id)
+    single_beers = JSON.parse(self.class.get("/beers/#{id}").body)
+    single_beers[0]
+  end
+
+  def random_beer
+    random_beers = JSON.parse(self.class.get("/beers/random").body)
+    random_beers[0]
+  end
+
 end
+
+punk_service = PunkService.new
+# p punk_service.all_beers
+# p punk_service.single_beer(1)
+p punk_service.random_beer
--- spec/punk_spec.rb
+++ spec/punk_spec.rb
@@ -1,1 +1,10 @@
 require 'spec_helper'
+
+describe "punk api" do
+
+  before(:each) do
+    service = PunkService.new
+    @api_tests = service.single_beer(1)
+  end
+
+end
