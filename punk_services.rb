4cb861a15625166c51cd511f2b23ba6167f4e6f2
LukeH1993 <lwheudebourck@gmail.com>
2017-10-23 17:07:14 +0100

    Added parameters and pagination

--- punk_services.rb
+++ punk_services.rb
@@ -2,9 +2,44 @@ require 'httparty'
 require 'json'
 
 class PunkService
+
   include HTTParty
 
   base_uri 'https://api.punkapi.com/v2'
+
+  def initialize
+    @uri = ""
+  end
+
+  # &&&
+  def parameters(options = {})
+    if options.empty? == false
+      @uri << "?"
+      @uri
+    options.each do |k, v|
+      @uri << "#{k} = #{v}"
+      query_response = JSON.parse(self.class.get('/beers#{uri}').body)
+      response[0]
+      end
+    else
+      parameters = JSON.parse(self.class.get('/beers').body)
+      parameters[0]
+    end
+  end
+
+  def pagination(options = {})
+    if options.empty? == false
+      @uri << "?"
+    options.each do |k, v|
+      @uri << "#{k} = #{v}"
+      query_response = JSON.parse(self.class.get('/beers#{uri}').body)
+      response[0]
+      end
+    else
+      pagination = JSON.parse(self.class.get('/beers').body)
+      pagination[0]
+    end
+  end
 
   def all_beers
     JSON.parse(self.class.get("/beers").body)
@@ -25,5 +60,7 @@ end
 punk_service = PunkService.new
 # p punk_service.all_beers
 # p punk_service.single_beer(1)
-p punk_service.random_beer
+# p punk_service.random_beer
 # p punk_service.pagination
+punk_service.parameters({"yeast" => "american", "abv" => 22})
+puts punk_service.all_beers
--- spec/punk_spec.rb
+++ spec/punk_spec.rb
@@ -7,4 +7,12 @@ describe "punk api" do
     @api_tests = service.single_beer(1)
   end
 
+  # it "Alcohol By Volume must be greater than 1 and less than 55" do
+  #   expect(@api_tests["abv"]).to be_between(1, 55)
+  # end
+
+  # it "International Bitterness Units must be greater than 1 and less than 150" do
+  #   expect(@api_tests["ibu"]).to be be_between(1, 150) unless(nil)
+  # end
+
 end
